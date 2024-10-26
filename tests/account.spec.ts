3fa2751e2bf46120d9cd8fdd745d99952d62bd7c
liznyamu <liznyamu@gmail.com>
2024-10-26 13:46:32 +0300

    updated test name

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('My Account', () => {
-  test('Access Orders List', async ({ page }) => {
+  test('Access Orders Link', async ({ page }) => {
     await page.goto('/my-account')
     await page.locator(`li a[href*='orders']`).click()
     await expect(page).toHaveURL(/.*orders/)
   });
 
-  test('Access Downloads', async ({ page }) => {
+  test('Access Downloads Link', async ({ page }) => {
     await page.goto('/my-account')
     await page.locator(`li a[href*='downloads']`).click()
     await expect(page).toHaveURL(/.*downloads/)
